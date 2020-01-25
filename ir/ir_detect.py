6da0c05442f700b9fdf1bc2c98b1c31605da3ab6
AnkitDimri <ankitdimri1999@gmail.com>
2020-01-25 23:46:29 +0000

    converted to function

--- ir/ir_detect.py
+++ ir/ir_detect.py
@@ -2,12 +2,14 @@ import RPi.GPIO as IO
 import sys
 sys.path.append('./picam')
 import capture as pic
-IO.setwarnings(False)
-IO.setmode(IO.BOARD)
-IO.setup(8, IO.IN)
-IO.setup(3, IO.OUT)
+
 
 def detect():
+    
+    IO.setwarnings(False)
+    IO.setmode(IO.BCM)
+    IO.setup(8, IO.IN)
+    IO.setup(3, IO.OUT)
     while 1:
         if(IO.input(8) == False):
             print("Motion detected !!")
@@ -16,3 +18,4 @@ def detect():
             print ("Clicking Pictures !!")
             pic.capture_images()
             print ("Pictures clicked !!")
+            break

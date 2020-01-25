364b60509a7d38b004d13195961ed004d578a273
AnkitDimri <ankitdimri1999@gmail.com>
2020-01-25 23:46:09 +0000

    modified for summation

--- detection/detect.py
+++ detection/detect.py
@@ -1,39 +1,43 @@
 from clarifai.rest import ClarifaiApp ,Video
 import unicodedata
-count = 0
-app = ClarifaiApp(api_key="1ed137256fde4eb08dc79847c0506eef")
+
+def detect_animals ():
+    count = 0
+    app = ClarifaiApp(api_key="1ed137256fde4eb08dc79847c0506eef")
 
 
 
 
 
-#general model
-model1 = app.public_models.general_model
-# givefilename
-#response = model.predict_by_filename('/home/som/Desktop/leopard.jpg')
+    #general model
+    model1 = app.public_models.general_model
+    # givefilename
+    #response = model.predict_by_filename('/home/som/Desktop/leopard.jpg')
 
-#if url is given
-#response = model.predict_by_url(url='https://i.pinimg.com/236x/db/9e/5c/db9e5ce4fe74df303529e02345584b42--habitats-state-parks.jpg')
-#model1 = app.models.get('animals')
-#model1.model_version = '0b3770d9cc8e47b7b1d3799831b31c1e'
+    #if url is given
+    #response = model.predict_by_url(url='https://i.pinimg.com/236x/db/9e/5c/db9e5ce4fe74df303529e02345584b42--habitats-state-parks.jpg')
+    #model1 = app.models.get('animals')
+    #model1.model_version = '0b3770d9cc8e47b7b1d3799831b31c1e'
 
-custom_response = [0 for i in range(1,11)]
-for i in range (1,11):
-    custom_response[i-1] = model1.predict_by_filename('../ir/Picture' + str(i) + '.jpg')
-#custom_response = model1.predict_by_url(url='https://i1.wp.com/npnews24.com/wp-content/uploads/2018/12/image.png')
+    custom_response = [0 for i in range(1,11)]
+    for i in range (1,11):
+        custom_response[i-1] = model1.predict_by_filename('Picture' + str(i) + '.jpg')
+        #custom_response = model1.predict_by_url(url='https://i1.wp.com/npnews24.com/wp-content/uploads/2018/12/image.png')
 
 
-#print(custom_response)
-#model = app.models.get('general-v1.3')
-#video = Video(filename='/home/som/Downloads/video1.mp4')
+    #print(custom_response)
+    #model = app.models.get('general-v1.3')
+    #video = Video(filename='/home/som/Downloads/video1.mp4')
 
-#print(response.encode)
+    #print(response.encode)
 
-#concepts = response['outputs'][0]['data']['concepts']
-for i in range (10):
-    concepts2 = custom_response[i]['outputs'][0]['data']['concepts']
-    for concept in concepts2:
-        if concept['name'] == 'animal' and concept["value"] >= 0.70 or concept['name'] == 'cheetah' and concept["value"] >= 0.70 or concept['name'] == 'leopard' and concept["value"] >= 0.70 or concept['name'] == 'elephant' and concept["value"] >= 0.70 or concept['name'] == 'mammal' and concept["value"] >= 0.70 or concept['name'] == 'wildlife' and concept["value"] >= 0.70 :
-           print(concept['name'].encode('ascii','ignore'),concept['value']) 
-           count = count+1
-           print(count)
+    #concepts = response['outputs'][0]['data']['concepts']
+    for i in range (10):
+        concepts2 = custom_response[i]['outputs'][0]['data']['concepts']
+        for concept in concepts2:
+            if concept['name'] == 'animal' and concept["value"] >= 0.70 or concept['name'] == 'cheetah' and concept["value"] >= 0.70 or concept['name'] == 'leopard' and concept["value"] >= 0.70 or concept['name'] == 'elephant' and concept["value"] >= 0.70 or concept['name'] == 'mammal' and concept["value"] >= 0.70 or concept['name'] == 'wildlife' and concept["value"] >= 0.70 :
+               print(concept['name'].encode('ascii','ignore'),concept['value']) 
+               count = count+1
+               #print(count)
+    
+    return count
